69d660bc2e1b1d1c54b09d24ba12bf8a8ce0d4f2
upendra <joshiupendra34@gmail.com>
2024-06-09 09:46:35 +0530

    added new feature

--- src/main/java/com/uj/graphql/controller/BookQueryController.java
+++ src/main/java/com/uj/graphql/controller/BookQueryController.java
@@ -1,5 +1,7 @@
 package com.uj.graphql.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.uj.graphql.service.GraphQLService;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
@@ -7,18 +9,33 @@ import com.uj.graphql.entity.Book;
 import com.uj.graphql.repository.BookRepository;
 import com.uj.graphql.types.BookFilter;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
 public class BookQueryController {
 
     private final BookRepository bookRepository;
+    private final GraphQLService graphQLService;
 
-    public BookQueryController(BookRepository bookRepository) {
+    public BookQueryController(BookRepository bookRepository, GraphQLService graphQLService) {
         this.bookRepository = bookRepository;
+        this.graphQLService = graphQLService;
     }
 
     @QueryMapping
@@ -41,6 +58,10 @@ public class BookQueryController {
         return Mono.just(bookRepository.findBookByAuthorAndTitle(author, title));
     }
 
+    @QueryMapping
+    public Flux<Book> getBookWithNoTitle(){
+        return Flux.fromIterable(bookRepository.findAll()).filter(b->b.getTitle().isBlank());
+    }
     @MutationMapping
     public Mono<Book> addBook(@Argument("filter") BookFilter bookFilter) {
         Book book = new Book();
@@ -65,5 +86,42 @@ public class BookQueryController {
         return Mono.empty();
 
     }
+
+    @GetMapping("/person/{personId}")
+    @ResponseBody
+    public Mono<JsonNode> getPersonDetail(@PathVariable String personId) throws IOException {
+        String personQuery = new String(Files.readAllBytes(Paths.get("src/main/resources/graphql/person.gql")));
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("personId", personId);
+        String path = "person";
+        return graphQLService.executeGraphQLQuery(personQuery, variables, path);
+    }
+
+    public static void downloadFile(String fileDownloadUri, String destinationPath) throws IOException {
+        URL url = new URL(fileDownloadUri);
+
+        try (InputStream in = url.openStream()) {
+            Path destination = Path.of(destinationPath);
+            Files.copy(in, destination, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/v1/getAllUsers")
+    public Flux<JsonNode> getAllUsers(){
+        String query = """
+                query{
+                  getAllUsers {
+                    username
+                    email
+                    csid
+                    employeeEmailAddress
+                  }
+                }
+                """;
+        return graphQLService.getAllUsers(query).flatMapMany(Flux::fromIterable);
+    }
+
 
 }
