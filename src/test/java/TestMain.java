0161ab3196106f2e0fbc32ac58e35c6189de930b
Upendra Joshi <joshiupendra34@gmail.com>
2026-02-01 11:11:59 +0530

    fixed the graphql issue

--- src/test/java/TestMain.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class TestMain {
-    public static void main(String[] args) {
-        String input = "4111111111111111";
-        char[] s = input.toCharArray();
-        System.out.println(s);
-    }
-}
